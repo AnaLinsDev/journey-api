19d50bdada8f30fea7e13ebd35d1089c8da56ca5
AnaLinsDev <analinsdev@gmail.com>
2026-09-04 18:45:33 -0300

    add the delete trip by id endpoint

--- src/Journey.API/Controllers/TripsController.cs
+++ src/Journey.API/Controllers/TripsController.cs
@@ -1,4 +1,5 @@
-﻿using Journey.Application.UseCases.Trips.GetAll;
+﻿using Journey.Application.UseCases.Trips.DeleteTrip;
+using Journey.Application.UseCases.Trips.GetAll;
 using Journey.Application.UseCases.Trips.GetById;
 using Journey.Application.UseCases.Trips.Register;
 using Journey.Communication.Requests;
@@ -42,5 +43,17 @@ public class TripsController : ControllerBase
         var response = useCase.Execute(id);
 
         return Ok(response);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    public IActionResult Delete([FromRoute] Guid id)
+    {
+        var useCase = new DeleteTripUseCase();
+        useCase.Execute(id);
+
+        return NoContent();
     }
 }

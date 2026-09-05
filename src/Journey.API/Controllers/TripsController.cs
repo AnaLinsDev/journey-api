5d266f485e92a1c0905fb3d37b93949e643f2378
AnaLinsDev <analinsdev@gmail.com>
2026-09-05 15:26:57 -0300

    add endpoint for register a new activity

--- src/Journey.API/Controllers/TripsController.cs
+++ src/Journey.API/Controllers/TripsController.cs
@@ -1,4 +1,5 @@
-﻿using Journey.Application.UseCases.Trips.DeleteTrip;
+﻿using Journey.Application.UseCases.Activities.Register;
+using Journey.Application.UseCases.Trips.DeleteTrip;
 using Journey.Application.UseCases.Trips.GetAll;
 using Journey.Application.UseCases.Trips.GetById;
 using Journey.Application.UseCases.Trips.Register;
@@ -13,8 +14,7 @@ public class TripsController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseShortTripJson), StatusCodes.Status201Created)]
-    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestRegisterTripJson request)
     {
         var useCase = new RegisterTripUseCase();
@@ -36,7 +36,7 @@ public class TripsController : ControllerBase
     [HttpGet]
     [Route("{id}")]
     [ProducesResponseType(typeof(ResponseTripJson), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
     public IActionResult GetById([FromRoute] Guid id)
     {
         var useCase = new GetTripByIdUseCase();
@@ -48,12 +48,27 @@ public class TripsController : ControllerBase
     [HttpDelete]
     [Route("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
     public IActionResult Delete([FromRoute] Guid id)
     {
         var useCase = new DeleteTripUseCase();
         useCase.Execute(id);
 
         return NoContent();
+    }
+
+    [HttpPost]
+    [Route("{tripId}/activity")]
+    [ProducesResponseType(typeof(ResponseShortTripJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+    public IActionResult RegisterActivity(
+        [FromRoute] Guid tripId,
+        [FromBody] RequestRegisterActivityJson request)
+    {
+        var useCase = new RegisterActivityForTripUseCase();
+        var response = useCase.Execute(tripId, request);
+
+        return Created(string.Empty, response);
     }
 }
--- src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripUseCase.cs
@@ -1,8 +1,6 @@
 ﻿using Journey.Communication.Requests;
 using Journey.Communication.Responses;
-using Journey.Exception;
 using Journey.Exception.ExceptionsBase;
-using Journey.Infrastructure;
 using Journey.Infrastructure;
 using Journey.Infrastructure.Entities;
 
